6c6d3cd6572bf80bb839f7d74b73af4635ef86cb
Michael Nakonechny <michaelnako@icloud.com>
2022-08-26 10:54:59 +0200

    add list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import ShoppingItem from "./ShoppingItem";
+
+const shoppingList = [
+  { name: "Potion", id: "1" },
+  { name: "Pokeball", id: "2" },
+  { name: "Para Healer", id: "3" },
+  { name: "Superball", id: "4" },
+  { name: "Masterball", id: "5" },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Pokemon Shopping List </h1>
       </header>
+      {/* function ShoppingItem({(countItem, onAddItem)}) {} */}
+      <ul>
+        {shoppingList.map((shopping) => (
+          <ShoppingItem key={shopping.id} name={shopping.name} />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/ShoppingItem.js
@@ -0,0 +1,11 @@
+export default function ShoppingItem({ id, name, onRemoveItem }) {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" />
+        {name}
+      </label>
+      <button onClick={() => onRemoveItem(id)}>Remove</button>
+    </li>
+  );
+}
